b0faec041a32592449902575603252a4415d6086
real-tea <akash.0711a@gmail.com>
2023-07-23 12:25:47 +0530

    file restructuring ♻

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import { Configuration , OpenAIApi } from 'openai';
+import { useState } from 'react';
 
 function App() {
+
+  const [isLoading , setIsLoading] = useState(true);
+  const [inputText , setInputText] = useState("");
+
+  const API_KEY = process.env.API_KEY;
+  const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+  const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET
+  const openai = new OpenAIApi(new Configuration({apiKey : API_KEY}));
+
+
+
+  const EXAMPLES = [
+    "I like Kanye West for my gym songs",
+    "I've been feeling like a Shakespearean Villain lately",
+    "Smitten by this lady i met in the metro"
+  ]
+
+  const handleEnterKey = async(e) => {
+    if(e.key === 'Enter' || e.type === 'click'){
+      if(e.preventDefault){
+        e.preventDefault();
+      }
+      setIsLoading(true);
+
+      let searchInput = inputText;
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
